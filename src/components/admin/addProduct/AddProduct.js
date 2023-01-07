ab70008705df8ebfeb86d92421a3a1b0257fbadf
SaidovAsadbek <asadbeksaidov011@gmail.com>
2023-01-07 12:53:08 +0500

    viewProduct.js changed

--- src/components/admin/addProduct/AddProduct.js
+++ src/components/admin/addProduct/AddProduct.js
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import Card from "../../Card/Card";
 import styles from "./AddProduct.module.scss";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-import { storage } from "../../../firebase/config";
+import { storage, db } from "../../../firebase/config";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import Loader from "../../Loader/Loader";
 
 const categories = [
     { id: 1, name: "Laptops" },
@@ -12,17 +15,22 @@ const categories = [
     { id: 4, name: "Phones" },
 ];
 
+const initialState = {
+    name: "",
+    imageUrl: "",
+    desc: "",
+    price: 0,
+    brand: "",
+    category: "",
+};
+
 const AddProduct = () => {
-    const [product, setProduct] = useState({
-        name: "",
-        imageUrl: "",
-        desc: "",
-        price: 0,
-        brand: "",
-        category: "",
-    });
+    const [product, setProduct] = useState({ ...initialState });
 
     const [uploadProgress, setUploadProgress] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate();
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -60,118 +68,148 @@ const AddProduct = () => {
     const addProduct = (e) => {
         e.preventDefault();
         console.log(product);
+
+        setIsLoading(true);
+
+        // Add product
+        try {
+            const docRef = addDoc(collection(db, "products"), {
+                name: product.name,
+                imageUrl: product.imageUrl,
+                desc: product.desc,
+                price: Number(product.price),
+                brand: product.brand,
+                category: product.category,
+                createdAt: Timestamp.now().toDate(),
+            });
+
+            setIsLoading(false);
+            setUploadProgress(0);
+            setProduct({ ...initialState });
+
+            toast.success("Product added successfully");
+            navigate("/admin/all-products");
+        } catch (error) {
+            setIsLoading(false);
+            toast.error(error.message);
+        }
     };
 
     return (
-        <div className={styles.product}>
-            <h1>Add New Product</h1>
-            <Card cardClass={styles.card}>
-                <form onSubmit={addProduct}>
-                    <label htmlFor="">
-                        Product Name:
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Product Name"
-                            required
-                            value={product.name}
-                            onChange={(e) => handleInputChange(e)}
-                        />
-                    </label>
-
-                    <label htmlFor="">
-                        Product Image:
-                        <Card className={styles.group}>
-                            {uploadProgress === 0 ? null : (
-                                <div className={styles.progress}>
-                                    <div
-                                        style={{ width: `${uploadProgress}%` }}
-                                        className={styles["progress-bar"]}>
-                                        {uploadProgress < 100
-                                            ? `Uploading ${uploadProgress}%`
-                                            : `Upload Complete ${uploadProgress}%`}
-                                    </div>
-                                </div>
-                            )}
-
+        <>
+            {isLoading && <Loader />}
+            <div className={styles.product}>
+                <h1>Add New Product</h1>
+                <Card cardClass={styles.card}>
+                    <form onSubmit={addProduct}>
+                        <label htmlFor="">
+                            Product Name:
                             <input
-                                type="file"
-                                name="image"
-                                accept="image/*"
-                                placeholder="Product Image"
-                                onChange={(e) => handleImageChange(e)}
+                                type="text"
+                                name="name"
+                                placeholder="Product Name"
+                                required
+                                value={product.name}
+                                onChange={(e) => handleInputChange(e)}
                             />
-                            {product.imageUrl === "" ? null : (
+                        </label>
+
+                        <label htmlFor="">
+                            Product Image:
+                            <Card className={styles.group}>
+                                {uploadProgress === 0 ? null : (
+                                    <div className={styles.progress}>
+                                        <div
+                                            style={{
+                                                width: `${uploadProgress}%`,
+                                            }}
+                                            className={styles["progress-bar"]}>
+                                            {uploadProgress < 100
+                                                ? `Uploading ${uploadProgress}%`
+                                                : `Upload Complete ${uploadProgress}%`}
+                                        </div>
+                                    </div>
+                                )}
+
                                 <input
-                                    type="text"
-                                    name="imageUrl"
-                                    value={product.imageUrl}
-                                    disabled
+                                    type="file"
+                                    name="image"
+                                    accept="image/*"
+                                    placeholder="Product Image"
+                                    onChange={(e) => handleImageChange(e)}
                                 />
-                            )}
-                        </Card>
-                    </label>
+                                {product.imageUrl === "" ? null : (
+                                    <input
+                                        type="text"
+                                        name="imageUrl"
+                                        value={product.imageUrl}
+                                        disabled
+                                    />
+                                )}
+                            </Card>
+                        </label>
 
-                    <label htmlFor="">
-                        Product Price:
-                        <input
-                            type="text"
-                            name="price"
-                            placeholder="Product Price"
-                            required
-                            value={product.price}
-                            onChange={(e) => handleInputChange(e)}
-                        />
-                    </label>
+                        <label htmlFor="">
+                            Product Price:
+                            <input
+                                type="text"
+                                name="price"
+                                placeholder="Product Price"
+                                required
+                                value={product.price}
+                                onChange={(e) => handleInputChange(e)}
+                            />
+                        </label>
 
-                    <label htmlFor="">
-                        Product Category:
-                        <select
-                            name="category"
-                            required
-                            value={product.category}
-                            onChange={(e) => handleInputChange(e)}>
-                            <option value="" disabled>
-                                Choose Products Category
-                            </option>
-                            {categories.map((cat, index) => {
-                                return (
-                                    <option value={cat.name} key={cat.id}>
-                                        {cat.name}
-                                    </option>
-                                );
-                            })}
-                        </select>
-                    </label>
+                        <label htmlFor="">
+                            Product Category:
+                            <select
+                                name="category"
+                                required
+                                value={product.category}
+                                onChange={(e) => handleInputChange(e)}>
+                                <option value="" disabled>
+                                    Choose Products Category
+                                </option>
+                                {categories.map((cat, index) => {
+                                    return (
+                                        <option value={cat.name} key={cat.id}>
+                                            {cat.name}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </label>
 
-                    <label htmlFor="">
-                        Product Company or Brand:
-                        <input
-                            type="text"
-                            name="brand"
-                            placeholder="Product Brand"
-                            required
-                            value={product.brand}
-                            onChange={(e) => handleInputChange(e)}
-                        />
-                    </label>
+                        <label htmlFor="">
+                            Product Company or Brand:
+                            <input
+                                type="text"
+                                name="brand"
+                                placeholder="Product Brand"
+                                required
+                                value={product.brand}
+                                onChange={(e) => handleInputChange(e)}
+                            />
+                        </label>
 
-                    <label htmlFor="">
-                        Product Description:
-                        <textarea
-                            required
-                            name="desc"
-                            value={product.desc}
-                            onChange={(e) => handleInputChange(e)}
-                            cols="30"
-                            rows="10"></textarea>
-                    </label>
-                    <button type="submit" className="--btn --btn-primary">
-                        Save Product
-                    </button>
-                </form>
-            </Card>
-        </div>
+                        <label htmlFor="">
+                            Product Description:
+                            <textarea
+                                required
+                                name="desc"
+                                value={product.desc}
+                                onChange={(e) => handleInputChange(e)}
+                                cols="30"
+                                rows="10"></textarea>
+                        </label>
+                        <button type="submit" className="--btn --btn-primary">
+                            Save Product
+                        </button>
+                    </form>
+                </Card>
+            </div>
+        </>
     );
 };
 
--- src/components/admin/viewProduct/ViewProduct.js
+++ src/components/admin/viewProduct/ViewProduct.js
@@ -1,7 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./ViewProduct.module.scss";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../../../firebase/config";
+import { toast } from "react-toastify";
+import { NavLink } from "react-router-dom";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import Loader from "../../Loader/Loader";
 
 const ViewProduct = () => {
-    return <div>ViewProduct</div>;
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    const getProducts = () => {
+        setIsLoading(true);
+
+        try {
+            const productsRef = collection(db, "products");
+            const q = query(productsRef, orderBy("createdAt", "desc"));
+
+            const unsubscribe = onSnapshot(q, (snapshot) => {
+                // console.log(snapshot.docs)
+                const allProducts = snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+
+                setProducts(allProducts);
+                setIsLoading(false);
+            });
+        } catch (error) {
+            setIsLoading(false);
+            toast.error(error.message);
+        }
+    };
+
+    return (
+        <>
+            {isLoading && <Loader />}
+            <div className={styles.table}>
+                <h2>All Products</h2>
+                {products.length === 0 ? (
+                    <p>No Products Found.</p>
+                ) : (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>s/n</th>
+                                <th>Image</th>
+                                <th>Name</th>
+                                <th>Category</th>
+                                <th>Price</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products.map((product, index) => {
+                                const {
+                                    id,
+                                    name,
+                                    imageUrl,
+                                    desc,
+                                    brand,
+                                    category,
+                                    price,
+                                } = product;
+
+                                return (
+                                    <tr key={id}>
+                                        <td>{index + 1}</td>
+                                        <td>
+                                            <img
+                                                src={imageUrl}
+                                                alt={name}
+                                                style={{ width: "100px" }}
+                                            />
+                                        </td>
+                                        <td>{name}</td>
+                                        <td>{category}</td>
+                                        <td>{`$${price}`}</td>
+                                        <td>
+                                            <NavLink to="/admin/add-products">
+                                                <FaEdit
+                                                    size={20}
+                                                    color="green"
+                                                />
+                                            </NavLink>
+                                            &nbsp;
+                                            <FaTrashAlt size={18} color="red" />
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default ViewProduct;
